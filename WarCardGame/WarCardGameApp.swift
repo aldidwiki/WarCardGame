fce2c37b2a7698415ee60ef485948f094209c258
Aldi Prahasta <aldidwiki@gmail.com>
2022-04-09 10:31:15 +0700

    initial commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Aldi Dwiki Prahasta on 09/04/22.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
